8755ebca2101e18a59dfe3a816f6fef7c5df19da
Na Li <nli@albany.edu>
2017-04-26 17:55:56 -0400

    Complete the insert method which inserts a node to the tree

--- Tree.java
+++ Tree.java
@@ -8,7 +8,9 @@ public class Tree{
     size = 0;
   }
   /*
-   * the search method that finds and returns the node that matches a search key 
+   * The search method that finds and returns the node that matches a search key 
+   * @param str the possible value of node
+   * @return the found node which has the value of the paramter
    */
   public Node search(String str){
     Node temp = root;
@@ -27,5 +29,36 @@ public class Tree{
       }
     }
     return temp;
+  }
+  /*
+   * the insert method which inserts a node to a tree
+   * @param n the node will be add to the tree
+   */
+  public void insert(Node n){
+    if(this.isEmpty()){
+      root = n;
+    }
+    else{
+      Node temp = root;
+      while(temp!=null)
+      {
+        if(temp.getValue().compareTo(n.getValue())>0){
+          temp = temp.getRight();
+        }
+        else if(temp.getValue().compareTo(n.getValue())<0){
+          temp = temp.getLeft();
+        }
+        else{
+          if(temp.getValue().compareTo(n.getValue())<0)
+          temp.setLeft(n);
+          else
+          temp.setRight(n);
+        }
+      }
+    }
+  }
+  //Empty method
+  public boolean isEmpty(){
+    return root == null;
   }
 }
