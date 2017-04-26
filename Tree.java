c9fd4e18bdd57f878112c484aa8a4fa9d77a31a4
Na Li <nli@albany.edu>
2017-04-25 23:50:02 -0400

    Complete the Search method of the Tree class

--- Tree.java
+++ Tree.java
@@ -7,5 +7,25 @@ public class Tree{
     root = null;
     size = 0;
   }
-  
+  /*
+   * the search method that finds and returns the node that matches a search key 
+   */
+  public Node search(String str){
+    Node temp = root;
+    boolean found = false;
+    while(!found){
+      if(temp.getValue() == str){
+        found = true;
+      }
+      else {
+        if(temp.getLeft() != null){
+          temp = temp.getLeft();
+        }
+        else{
+          temp = temp.getRight();
+        }
+      }
+    }
+    return temp;
+  }
 }
